38640d73d1b4cc77ae091535fcc0e01eb9dd134f
winnie-webb <wbrown152020@gmail.com>
2020-12-17 19:19:48 -0800

    Added cards.json to refactor code base

--- README.md
+++ README.md
@@ -3,16 +3,20 @@ This project is for the company eternal-tours.
 It encompasses the fundamental features of a tour website and must contain great visual effects
 
 Overview : 
+Home/Tours Page
 1. A basic header with a basic hero image and links along with a button for discovering tours
 2. A small section with a video with text/slogan on it 
 3. A gallery with many images of the various destinations
 4. Cards that contain info on the many different tours and prices. It will be a slider with the use of swiper library
 5. Footer with my referal and the links
 
+Airport Transfers Page : 
+1. A basic header with a basic hero image and links and slogan/company name
+2. 
 Features :
 
-2. Users should be able to subscribe.
+1. Users should be able to subscribe.
 
 Future Add-ons :
-1. Users should be able to book tour online. This feature will use stripe payment.
+2. Users should be able to book tour online. This feature will use stripe payment.
 
--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"public/views"))
+
 app.get("/",(req,res) => {
-    res.render("index")
+   const cardsJSON = fs.readFileSync(`${__dirname}/public/views/cards.json`);
+   const cardsObject = JSON.parse(cardsJSON);
+   const cards = cardsObject.cards;
+    res.render("index",{cards: cards})
 })
+app.get("/airport-transfer",(req,res) => res.render("transfer"))
 app.get("*",(req,res) => res.send(`<h1 style="text-align:center; font-family : sans-serif">Not Completed Only the home page design is completed</h1>`))
+
 app.listen(PORT,() => console.log("Server has started"))
 
 //  Setup Nodemailer 
