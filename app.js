b9db8bc56a435608b7f147405d55b829cb2c51c1
winnie-webb <wbrown152020@gmail.com>
2020-12-20 08:31:33 -0800

    Added Animations to Hotel cards

--- app.js
+++ app.js
@@ -19,7 +19,16 @@ app.get("/",(req,res) => {
 app.get("/airport-transfer",(req,res) => {
 const hotelsJSON = fs.readFileSync(`${__dirname}/public/views/hotels.json`);
 const hotelsObject = JSON.parse(hotelsJSON);
-const hotels = hotelsObject.hotels;
+const hotels = hotelsObject.hotels.sort((a,b) => {
+  const nameA = a.name.toLowerCase();
+  const nameB = b.name.toLowerCase();
+
+  if(nameA < nameB) return -1;
+  if(nameB < nameA) return 1;
+  
+  return 0;
+});
+
   res.render("transfer",{hotels:hotels});
 })
 app.get("/hotels",(req,res) => res.json(hotels))
