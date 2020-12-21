bf744187ac3f09323d1f8939343cabb7cf097a19
winnie-webb <wbrown152020@gmail.com>
2020-12-20 19:23:09 -0800

    Fixed Bug for toggle

--- public/js/transfer.js
+++ public/js/transfer.js
@@ -12,23 +12,17 @@ const hotelHideAndShowFuntionality = ( () => {
 
     const hotelsContainer = document.querySelectorAll(".hotels__hotel");
     const hotelsDescription = document.querySelectorAll(".hotels__hotel__description");
-    const rightArrow = document.querySelectorAll(".right-arrow");
+    const rightArrows = document.querySelectorAll(".right-arrow");
     
-    let i = 1;
     hotelsContainer.forEach((hotelContainer,index) => {
         // rightArrow[index].style.clipPath = "polygon(49% 92%, 10% 36%, 89% 35%)"
         hotelContainer.addEventListener("click",() => {
             
             hotelContainer.classList.toggle("hotels-height");
             hotelsDescription[index].classList.toggle("hotels-hide-description");
-            if( i === 1) {
-                rightArrow[index].style.clipPath = "polygon(10% 21%, 48% 89%, 94% 21%)";
-                i++
-            }
-            else  {
-                rightArrow[index].style.clipPath = "polygon(15% 14%, 15% 93%, 84% 57%)";
-                i = 1;
-            }
+
+            rightArrows[index].classList.toggle("down-arrow");
+            
         })
             
     })
