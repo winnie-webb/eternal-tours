bffa5eb850dcc196a89a458df970c75b9d9bcb04
winnie-webb <wbrown152020@gmail.com>
2020-12-27 15:21:28 -0800

    Added Responsive Header

---
+++ public/js/header.js
@@ -0,0 +1,9 @@
+const toggleNavbar = 
+(() => {
+const burger = document.querySelector(".burger");
+const navbar = document.querySelector(".header__nav__links");
+burger.onclick = () => {
+    navbar.classList.toggle("navbar-toggle")
+}
+})
+toggleNavbar();
