45f4c2f4f10f6b9e9eff3fa3bf8eadb45c860c34
winnie-webb <wbrown152020@gmail.com>
2021-01-02 09:52:02 -0800

    Added animations to buttons

--- public/js/transfer.js
+++ public/js/transfer.js
@@ -1,36 +1,46 @@
-
+// Remember to make the first row already toggled
 const scrollFunctionality = (() => {
     const checkPricingsBtns = document.querySelectorAll(".card-pricings-btn");
     const topScrollerBtn = document.querySelector(".header__content-button");
-    const hotelsSection = document.querySelector(".hotels");
-    
+    const hotelsSection = document.querySelector(".right-arrow");
+    const hotels = document.querySelectorAll(".hotels__hotel");
     function scrollToHotels(){
         hotelsSection.scrollIntoView({behavior:"smooth"})
     }
+    for(let i = 0; i < 3; i++){
+        setTimeout(() => hotels[i].click(), 1000)
+    }
+
     checkPricingsBtns.forEach(btn => {
-        btn.addEventListener('click', scrollToHotels)
+        btn.addEventListener('click',() => {
+            setTimeout(() => hotels[0].click(), 1000)
+            setTimeout(() => hotels[0].click(), 2000)
+            scrollToHotels()
+        } )
     })
     topScrollerBtn.addEventListener('click',scrollToHotels) 
 })
+const hotel = document.querySelector(".hotels__hotel")
 
 const hotelHideAndShowFuntionality = ( () => {
 
     const hotelsContainer = document.querySelectorAll(".hotels__hotel");
     const hotelsDescription = document.querySelectorAll(".hotels__hotel__description");
     const rightArrows = document.querySelectorAll(".right-arrow");
-    
+
     hotelsContainer.forEach((hotelContainer,index) => {
-        // rightArrow[index].style.clipPath = "polygon(49% 92%, 10% 36%, 89% 35%)"
-        hotelContainer.addEventListener("click",() => {
-            
+        hotelContainer.addEventListener("click",e => {
             hotelContainer.classList.toggle("hotels-height");
             hotelsDescription[index].classList.toggle("hotels-hide-description");
-
+            // Stop Propagation for the content inside the cards 
+            hotelsDescription[index].addEventListener("click",e =>{
+                e.stopPropagation()
+            } )
             rightArrows[index].classList.toggle("down-arrow");
-            
         })
             
     })
 })
+
 scrollFunctionality();
 hotelHideAndShowFuntionality();
